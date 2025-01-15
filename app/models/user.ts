c9ad3a27cb91837ab9986b49bcce556c0c06efb4
MarionQH <marion.huet49@gmail.com>
2025-01-15 12:22:57 +0100

    feat:correction import

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, belongsTo, column, hasMany } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { Authenticator } from '@adonisjs/auth'
 import { Authenticators } from '@adonisjs/auth/types'
