ec89f7e4ce1d9df65bf95c3c62f8416ff6111f30
MarionQH <marion.huet49@gmail.com>
2025-01-16 11:33:24 +0100

    ajout text validator

---
+++ app/validators/text.ts
@@ -0,0 +1,9 @@
+import vine from '@vinejs/vine'
+
+export const openGraphsValidator = vine.compile(
+  vine.object({
+    name: vine.string(),
+    prefixUrl: vine.string(),
+    suffixUrl: vine.string(),
+  })
+)
