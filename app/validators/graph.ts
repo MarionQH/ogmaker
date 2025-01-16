6701605d94cf5f652423456c9a935b2c1eaf5734
MarionQH <marion.huet49@gmail.com>
2025-01-16 11:29:59 +0100

    feat:creation du form textline

--- app/validators/graph.ts
+++ app/validators/graph.ts
@@ -5,22 +5,5 @@ export const openGraphsValidator = vine.compile(
     name: vine.string(),
     prefixUrl: vine.string(),
     suffixUrl: vine.string(),
-    // userId: vine.number().isExists({ table: 'users', column: 'id' }),
-    // ogUrl: vine.string(),
-    // textline: vine
-    //   .array(
-    //     vine.object({
-    //       id: vine.number(),
-    //       openGhaphId: vine.number(),
-    //       text: vine.string(),
-    //       textPolice: vine.string(),
-    //       textSize: vine.string(),
-    //       textWeight: vine.string(),
-    //       textColor: vine.string(),
-    //       textLongitude: vine.string(),
-    //       textLatitude: vine.string(),
-    //     })
-    //   )
-    //   .optional(),
   })
 )
