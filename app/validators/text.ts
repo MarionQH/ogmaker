5c2bdf179bc4ebabf8e204780d7386fb8e0f2653
MarionQH <marion.huet49@gmail.com>
2025-01-28 12:21:44 +0100

    fix: regex text validator

--- app/validators/text.ts
+++ app/validators/text.ts
@@ -7,7 +7,7 @@ export const textValidator = vine.compile(
     textSize: vine.string().regex(/^(?:[1-9][0-9]|1[0-9]{2}|2[0-5][0-9]|260)$/),
     textWeight: vine.string().maxLength(100),
     textColor: vine.string().regex(/^#([0-9A-Fa-f]{6}|[0-9A-Fa-f]{8})$/),
-    textLongitude: vine.string().regex(/^(?:[1-9][0-9]?|[1-9][0-9]{2}|900)$/),
-    textLatitude: vine.string().regex(/^(?:[1-9][0-9]?|[1-7][0-3]{2})$/),
+    textLongitude: vine.string().regex(/^([1-9]|[1-9][0-9]|[1-8][0-9]{2}|900)$/),
+    textLatitude: vine.string().regex(/^([1-9]|[1-9][0-9]|[1-6][0-9]{2}|7[01][0-9]|72[0-5])$/),
   })
 )
